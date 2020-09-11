e4add84f526f3e6de8fbb336e898f4bb9835b0c6
davidfeldt <david@jazlabs,com>
2020-09-11 13:42:00 -0400

    Refactor password hashing for PHP 7.x

--- public_html/v3/include/DbHandler.php
+++ public_html/v3/include/DbHandler.php
@@ -1,17 +1,5 @@
 <?php
 
-// These constants may be changed without breaking existing hashes.
-	define("PBKDF2_HASH_ALGORITHM", "sha256");
-	define("PBKDF2_ITERATIONS", 50000);
-	define("PBKDF2_SALT_BYTE_SIZE", 24);
-	define("PBKDF2_HASH_BYTE_SIZE", 24);
-
-	define("HASH_SECTIONS", 4);
-	define("HASH_ALGORITHM_INDEX", 0);
-	define("HASH_ITERATION_INDEX", 1);
-	define("HASH_SALT_INDEX", 2);
-	define("HASH_PBKDF2_INDEX", 3);
-
 class DbHandler {
 
     private $conn;
@@ -23,99 +11,6 @@ class DbHandler {
         $db = new DbConnect();
         $this->conn = $db->connect();
     }
-
-    public function create_hash($password) {
-    	// format: algorithm:iterations:salt:hash
-		$salt = base64_encode(mcrypt_create_iv(PBKDF2_SALT_BYTE_SIZE, MCRYPT_DEV_URANDOM));
-		return PBKDF2_HASH_ALGORITHM . ":" . PBKDF2_ITERATIONS . ":" .  $salt . ":" .
-        	base64_encode($this->pbkdf2(
-            	PBKDF2_HASH_ALGORITHM,
-				$password,
-				$salt,
-				PBKDF2_ITERATIONS,
-				PBKDF2_HASH_BYTE_SIZE,
-				true
-			));
-	}
-
-	public function validate_password($password, $correct_hash) {
-    	$params = explode(":", $correct_hash);
-		if(count($params) < HASH_SECTIONS)
-			return false;
-		$pbkdf2 = base64_decode($params[HASH_PBKDF2_INDEX]);
-		return $this->slow_equals(
-        		$pbkdf2,
-				$this->pbkdf2(
-				$params[HASH_ALGORITHM_INDEX],
-				$password,
-				$params[HASH_SALT_INDEX],
-				(int)$params[HASH_ITERATION_INDEX],
-				strlen($pbkdf2),
-				true
-				));
-	}
-
-	// Compares two strings $a and $b in length-constant time.
-	private function slow_equals($a, $b) {
-    	$diff = strlen($a) ^ strlen($b);
-		for($i = 0; $i < strlen($a) && $i < strlen($b); $i++) {
-        	$diff |= ord($a[$i]) ^ ord($b[$i]);
-		}
-		return $diff === 0;
-	}
-
-	/*
-	 * PBKDF2 key derivation function as defined by RSA's PKCS #5: https://www.ietf.org/rfc/rfc2898.txt
-     * $algorithm - The hash algorithm to use. Recommended: SHA256
-     * $password - The password.
-     * $salt - A salt that is unique to the password.
-     * $count - Iteration count. Higher is better, but slower. Recommended: At least 1000.
-     * $key_length - The length of the derived key in bytes.
-     * $raw_output - If true, the key is returned in raw binary format. Hex encoded otherwise.
-     * Returns: A $key_length-byte key derived FROM the password and salt.
-     *
-     * Test vectors can be found here: https://www.ietf.org/rfc/rfc6070.txt
-     *
-     * This implementation of PBKDF2 was originally created by https://defuse.ca
-     * With improvements by http://www.variations-of-shadow.com
-     */
-
-	 private function pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output = false) {
-		 $algorithm = strtolower($algorithm);
-		 if(!in_array($algorithm, hash_algos(), true))
-		 	trigger_error('PBKDF2 ERROR: Invalid hash algorithm.', E_USER_ERROR);
-		 if($count <= 0 || $key_length <= 0)
-		 	trigger_error('PBKDF2 ERROR: Invalid parameters.', E_USER_ERROR);
-
-		 if (function_exists("hash_pbkdf2")) {
-			 // The output length is in NIBBLES (4-bits) if $raw_output is false!
-			 if (!$raw_output) {
-				 $key_length = $key_length * 2;
-			 }
-			 return hash_pbkdf2($algorithm, $password, $salt, $count, $key_length, $raw_output);
-		 }
-
-		 $hash_length = strlen(hash($algorithm, "", true));
-		 $block_count = ceil($key_length / $hash_length);
-
-		 $output = "";
-		 for($i = 1; $i <= $block_count; $i++) {
-			 // $i encoded as 4 bytes, big endian.
-			 $last = $salt . pack("N", $i);
-			 // first iteration
-			 $last = $xorsum = hash_hmac($algorithm, $last, $password, true);
-			 // perform the other $count - 1 iterations
-			 for ($j = 1; $j < $count; $j++) {
-				 $xorsum ^= ($last = hash_hmac($algorithm, $last, $password, true));
-			 }
-			 $output .= $xorsum;
-		 }
-
-		 if($raw_output)
-		 	return substr($output, 0, $key_length);
-		 else
-		 	return bin2hex(substr($output, 0, $key_length));
-	 }
 
    public function captureDebug($endpoint, $request, $response) {
     date_default_timezone_set($_ENV['TIMEZONE']);
@@ -593,7 +488,7 @@ class DbHandler {
     $username = $this->getUsernameFromResetCode($reset_code);
 
     if ($username) {
-      $password_hash = $this->create_hash($password);
+      $password_hash = password_hash($password, PASSWORD_DEFAULT);
 
       $stmt = $this->conn->prepare("UPDATE user SET password = :password, reset_code = '', reset_code_short = '', reset_code_active = '0', date_modified = NOW() WHERE username = :username");
 
@@ -916,7 +811,7 @@ table.list .center {
        	  } else {
 
             // Generating password hash
-            $password_hash = $this->create_hash($new_password);
+            $password_hash = password_hash($new_password, PASSWORD_DEFAULT);
 
             // Generating new API key
             //$api_key = $this->generateApiKey();
@@ -955,7 +850,7 @@ table.list .center {
             // Found user with the username
             // Now verify the password
 
-            if ($this->validate_password($password, $row['password'])) {
+            if ($this->password_verify($password,$row['password'])) {
                 // User password is correct
                 return 'valid';
             } else {
